dc252f0b8b43d8f3256b5ebd2a8ed779e408470a
Quique Fdez Guerra <quique@ckgrafico.com>
2013-12-21 00:56:58 +0100

    version 0.2
    added roaming data and in-app purchases

--- ExampleTools/ExampleTools/js/default.js
+++ ExampleTools/ExampleTools/js/default.js
@@ -1,72 +1,87 @@
 ﻿// Para obtener una introducción a la plantilla En blanco, consulte la siguiente documentación:
 // http://go.microsoft.com/fwlink/?LinkId=232509
 (function () {
-    "use strict";
+	"use strict";
 
-    var app = WinJS.Application;
-    var activation = Windows.ApplicationModel.Activation;
+	var app = WinJS.Application;
+	var activation = Windows.ApplicationModel.Activation;
 
-    app.onactivated = function (args) {
-        if (args.detail.kind === activation.ActivationKind.launch) {
-            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+	app.onactivated = function (args) {
+		if (args.detail.kind === activation.ActivationKind.launch) {
+			if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
 
-                var $doc = $(document);
+				var $doc = $(document);
 
-                // alert
-                $doc.on('click', '#1', function () {
-                    WinjsTools.alert('testing');
-                });
+				// alert
+				$doc.on('click', '#1', function () {
+					WinjsTools.alert('testing');
+				});
 
-                // confirm
-                $doc.on('click', '#2', function () {
-                    WinjsTools.confirm({
-                        text: 'testing',
-                        onAccept: function () { $('#2-extra').text('Accepted'); },
-                        onCancel: function () { $('#2-extra').text('Cancelled'); }
-                    });
-                });
+				// confirm
+				$doc.on('click', '#2', function () {
+					WinjsTools.confirm({
+						text: 'testing',
+						onAccept: function () { $('#2-extra').text('Accepted'); },
+						onCancel: function () { $('#2-extra').text('Cancelled'); }
+					});
+				});
 
-                // alertOptions
-                $doc.on('click', '#3', function () {
-                    WinjsTools.alertThree({
-                        text: 'testing',
-                        options: [
-                            {
-                                button: 'Option 1',
-                                callback: function () { $('#3-extra').text('Option 1'); }
-                            },
-                            {
-                                button: 'Option 2',
-                                callback: function () { $('#3-extra').text('Option 2'); }
-                            },
-                            {
-                                button: 'Option 3',
-                                callback: function () { $('#3-extra').text('Option 3'); }
-                            },
-                            { // only can three
-                                button: 'Option 4',
-                                callback: function () { $('#3-extra').text('Option 4'); }
-                            }
-                        ]
-                    });
-                });
-                
-                // alert
-                $doc.on('click', '#4', function () {
-                    WinjsTools.onlineAction(function () { $('#4-extra').text('Online'); }, 'Connect to internet');
-                });
+				// alertOptions
+				$doc.on('click', '#3', function () {
+					WinjsTools.alertThree({
+						text: 'testing',
+						options: [
+							{
+								button: 'Option 1',
+								callback: function () { $('#3-extra').text('Option 1'); }
+							},
+							{
+								button: 'Option 2',
+								callback: function () { $('#3-extra').text('Option 2'); }
+							},
+							{
+								button: 'Option 3',
+								callback: function () { $('#3-extra').text('Option 3'); }
+							},
+							{ // only can three
+								button: 'Option 4',
+								callback: function () { $('#3-extra').text('Option 4'); }
+							}
+						]
+					});
+				});
+				
+				// alert
+				$doc.on('click', '#4', function () {
+					WinjsTools.onlineAction(function () { $('#4-extra').text('Online'); }, 'Connect to internet');
+				});
+
+				// roaming data
+				$doc.on('click', '#5', function () {
+				    WinjsTools.roaming('test', 'Testing' + Math.random());
+					$('#5-extra').text(WinjsTools.roaming('test')); 
+				});
+
+				$('#5-extra').text(WinjsTools.roaming('test'));
+
+			    // purchase
+                // (fails because isn't in the store)
+				$doc.on('click', '#6', function () {
+				    WinjsTools.purchase({
+				        id: 'myPurchase',
+				        debugMode: true
+				    });
+				});
 
 
+			}
+			args.setPromise(WinJS.UI.processAll());
+		}
+	};
 
+	app.oncheckpoint = function (args) {
 
-            }
-            args.setPromise(WinJS.UI.processAll());
-        }
-    };
+	};
 
-    app.oncheckpoint = function (args) {
-
-    };
-
-    app.start();
+	app.start();
 })();
--- core/WinjsTools.js
+++ core/WinjsTools.js
@@ -1,179 +1,216 @@
 ﻿/*
-    Collection of tools for WinJS
-    author: @CKGrafico
-    version: 0.1
+	Collection of tools for WinJS
+	author: @CKGrafico
+	version: 0.2
 */
 
 
 (function (g, _) {
 
-    'use strict';
+	'use strict';
 
-    var networkInfo = Windows.Networking.Connectivity.NetworkInformation;
-    var networkConnectivityInfo = Windows.Networking.Connectivity.NetworkConnectivityLevel;
+	var networkInfo = Windows.Networking.Connectivity.NetworkInformation;
+	var networkConnectivityInfo = Windows.Networking.Connectivity.NetworkConnectivityLevel;
+	var currentApp = Windows.ApplicationModel.Store.CurrentApp;
+	var currentAppSimulator = Windows.ApplicationModel.Store.CurrentAppSimulator;
+	var licenseInformation = currentApp.licenseInformation;
+	var applicationData = Windows.Storage.ApplicationData.current;
+	var roamingSettings = applicationData.roamingSettings;
 
 
-    //Tools Class
-    var Tools = WinJS.Class.define(
+	//Tools Class
+	var Tools = WinJS.Class.define(
 
-        // Constructor
-        function () {
+		// Constructor
+		function () {
 
-        },
+		},
 
-        // Instance Members
-        {
+		// Instance Members
+		{
 
-            // Alert popup
-            alert: function (options) {
-                // defaults
-                var dafaults = {
-                    text: '',
-                    button: 'Accept',
-                    callback: function () { }
-                };
+			// Alert popup
+			alert: function (options) {
+				// defaults
+				var dafaults = {
+					text: '',
+					button: 'Accept',
+					callback: function () { }
+				};
 
-                // if only text
-                if (typeof options === 'string') {
-                    dafaults.text = options;
+				// if only text
+				if (typeof options === 'string') {
+					dafaults.text = options;
 
-                } else {
-                    // extend options
-                    _.extend(dafaults, options);
-                }
-                
-                this.alertThree({
-                    text: dafaults.text,
-                    options: [
-                        {
-                            button: dafaults.button,
-                            callback: dafaults.callback
-                        }
-                    ]
-                });
-            },
+				} else {
+					// extend options
+					_.extend(dafaults, options);
+				}
+				
+				this.alertThree({
+					text: dafaults.text,
+					options: [
+						{
+							button: dafaults.button,
+							callback: dafaults.callback
+						}
+					]
+				});
+			},
 
-            // Confirm popup
-            confirm: function (options) {
+			// Confirm popup
+			confirm: function (options) {
 
-                // defaults
-                var dafaults = {
-                    text: '',
-                    buttonAccept: 'Accept',
-                    onAccept: function () { },
-                    buttonCancel: 'Cancel',
-                    onCancel: function () { }
-                };
+				// defaults
+				var dafaults = {
+					text: '',
+					buttonAccept: 'Accept',
+					onAccept: function () { },
+					buttonCancel: 'Cancel',
+					onCancel: function () { }
+				};
 
-                // if only text
-                if (typeof options === 'string') {
-                    dafaults.text = options;
+				// if only text
+				if (typeof options === 'string') {
+					dafaults.text = options;
 
-                } else {
-                    // extend options
-                    _.extend(dafaults, options);
-                }
+				} else {
+					// extend options
+					_.extend(dafaults, options);
+				}
 
-                this.alertThree({
-                    text: dafaults.text,
-                    options: [
-                        {
-                            button: dafaults.buttonAccept,
-                            callback: dafaults.onAccept
-                        },
+				this.alertThree({
+					text: dafaults.text,
+					options: [
+						{
+							button: dafaults.buttonAccept,
+							callback: dafaults.onAccept
+						},
 
-                        {
-                            button: dafaults.buttonCancel,
-                            callback: dafaults.onCancel
-                        }
-                    ]
-                });
-            },
+						{
+							button: dafaults.buttonCancel,
+							callback: dafaults.onCancel
+						}
+					]
+				});
+			},
 
-            // Show modal window with 'x' options
-            alertThree: function (options) {
+			// Show modal window with 'x' options
+			alertThree: function (options) {
 
-                // defaults
-                var dafaults = {
-                    text: '',
-                    options: [
-                        {
-                            button: 'Ok',
-                            callback: function () { }
-                        }
-                    ]
-                };
+				// defaults
+				var dafaults = {
+					text: '',
+					options: [
+						{
+							button: 'Ok',
+							callback: function () { }
+						}
+					]
+				};
 
-                // if only text
-                if (typeof options === 'string') {
-                    dafaults.text = options;
+				// if only text
+				if (typeof options === 'string') {
+					dafaults.text = options;
 
-                } else {
-                    // extend options
-                    _.extend(dafaults, options);
-                }
+				} else {
+					// extend options
+					_.extend(dafaults, options);
+				}
 
-                var ShowingMessage;
-                if (!ShowingMessage) {
-                    ShowingMessage = true;
+				var ShowingMessage;
+				if (!ShowingMessage) {
+					ShowingMessage = true;
 
-                    setTimeout(function () {
+					setTimeout(function () {
 
-                        // Create the message dialog and set its content
-                        var msg = new Windows.UI.Popups.MessageDialog(dafaults.text);
+						// Create the message dialog and set its content
+						var msg = new Windows.UI.Popups.MessageDialog(dafaults.text);
 
-                        // Add commands and set their command handlers
-                        _.each(dafaults.options, function (option, key) {
-                            if (key < 3) {
-                                msg.commands.append(
-                                    new Windows.UI.Popups.UICommand(option.button, option.callback)
-                                );
-                            }
-                        });
+						// Add commands and set their command handlers
+						_.each(dafaults.options, function (option, key) {
+							if (key < 3) {
+								msg.commands.append(
+									new Windows.UI.Popups.UICommand(option.button, option.callback)
+								);
+							}
+						});
 
-                        msg.showAsync().done(function () {
-                            ShowingMessage = false;
-                        });
-                    });
-                }
-            },
+						msg.showAsync().done(function () {
+							ShowingMessage = false;
+						});
+					});
+				}
+			},
 
-            // Prompt popup
-            prompt: function (options) {
-                // to do
-            },
+			// Return true if you have internet
+			isOnline: function() {
+				var connectionProfile = networkInfo.getInternetConnectionProfile();
+				if (connectionProfile === null) {
+					return false;
+				}
 
-            // Return true if you have internet
-            isOnline: function() {
-                var connectionProfile = networkInfo.getInternetConnectionProfile();
-                if (connectionProfile === null) {
-                    return false;
-                }
+				var networkConnectivityLevel = connectionProfile.getNetworkConnectivityLevel();
+				if (networkConnectivityLevel === networkConnectivityInfo.none
+					|| networkConnectivityLevel === networkConnectivityInfo.localAccess
+					|| networkConnectivityLevel === networkConnectivityInfo.constrainedInternetAccess) {
+					return false;
+				}
 
-                var networkConnectivityLevel = connectionProfile.getNetworkConnectivityLevel();
-                if (networkConnectivityLevel === networkConnectivityInfo.none
-                    || networkConnectivityLevel === networkConnectivityInfo.localAccess
-                    || networkConnectivityLevel === networkConnectivityInfo.constrainedInternetAccess) {
-                    return false;
-                }
+				return true;
+			},
 
-                return true;
-            },
+			// Do an action if you have internet
+			onlineAction: function (callback, alertOptions) {
+				if(this.isOnline()) {
+					callback();
+				}else{
+					this.alert(alertOptions);
+				}
+			},
 
-            // Do an action if you have internet
-            onlineAction: function (callback, alertOptions) {
-                if(this.isOnline()) {
-                    callback();
-                }else{
-                    this.alert(alertOptions);
-                }
-            },
+			// Roaming data
+			roaming: function (key, data) {
+				if(data) {
+					 roamingSettings.values[key] = data;
+				}else{
+					 return roamingSettings.values[key];
+				}
+			},
 
-            // to do in-app purchases, roaming, etc..
-        }
-    );
+			// In-App purchases
+			purchase: function(options) {
 
-    // Export
-    g.WinjsTools = new Tools();
+				var dafaults = {
+					id: '',
+					debugMode: false,
+					onDone: function () { console.log('Done purchase ' + dafaults.id); },
+					onFail: function (results) { console.log('FAIL purchase ' + dafaults.id); }
+				};
+
+				_.extend(dafaults, options);
+
+				var app;
+				if(dafaults.debugMode) {
+					 app = currentAppSimulator;
+					 console.log('You\'re in debug mode for purchase with id: ' + dafaults.id);
+				}else{
+					app = currentApp;
+				}
+
+				app.requestProductPurchaseAsync(dafaults.id).done(
+					function (results) {
+						if (licenseInformation.productLicenses.lookup(dafaults.id).isActive){
+							 dafaults.onDone(results);
+						} else {
+							 dafaults.onFail(results);
+						}
+					});
+			}
+		}
+	);
+
+	// Export
+	g.WinjsTools = new Tools();
 
 })(this, _);
